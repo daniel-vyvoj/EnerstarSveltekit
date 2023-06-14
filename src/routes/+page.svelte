6b50dd5f722cc44005071591fd9680c316386ce6
Daniel Krcil <palmmusic.cz@gmail.com>
2023-06-14 13:46:31 +0000

    Src

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,194 +1,189 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import Chart from 'chart.js/auto';
-  
-  interface ChartData {
-    Hodina: string;
-    'Eur/MWH': number;
+import { onMount } from 'svelte';
+import Chart from 'chart.js/auto';
+
+let currentTime = getCurrentTime();
+
+function getCurrentTime() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
+}
+
+function transformChartData(data: any[]) {
+  const transformedData = [];
+  for (let i = 0; i < data.length - 1; i++) {
+    transformedData.push(data[i]);
+    transformedData.push({
+      Hodina: '',
+      'Eur/MWH': 0,
+    });
   }
-  
-  let currentTime = getCurrentTime();
-  
-  function getCurrentTime() {
-    const date = new Date();
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
-    return `${hours}:${minutes}:${seconds}`;
+  transformedData.push(data[data.length - 1]);
+  return transformedData;
+}
+
+let data: any[] = [];
+let dailyMin = '';
+let currentPrice = '';
+let dailyMax = '';
+
+onMount(async () => {
+  try {
+    const response = await fetch('/src/routes/DataCen.json');
+    const originalData = await response.json();
+    data = transformChartData(originalData);
+
+    dailyMin = calculateDailyMin(originalData);
+    currentPrice = calculateCurrentPrice(originalData);
+    dailyMax = calculateDailyMax(originalData);
+
+    renderChart();
+  } catch (error) {
+    console.error('Chyba při načítání dat:', error);
   }
-  
-  function transformChartData(data: any[]) {
-    const transformedData: ChartData[] = [];
-    for (let i = 0; i < data.length - 1; i++) {
-      transformedData.push(data[i]);
-      transformedData.push({
-        Hodina: '',
-        'Eur/MWH': 0,
-      });
-    }
-    transformedData.push(data[data.length - 1]);
-    return transformedData;
-  }
-  
-  let data: ChartData[] = [];
-  let dailyMin = '';
-  let currentPrice = '';
-  let dailyMax = '';
-  
-  onMount(async () => {
-    try {
-      const response = await fetch('/static/DataCen.json');
-      const originalData: ChartData[] = await response.json();
-      const transformedData: ChartData[] = transformChartData(originalData);
-  
-      dailyMin = calculateDailyMin(originalData);
-      currentPrice = calculateCurrentPrice(originalData);
-      dailyMax = calculateDailyMax(originalData);
-  
-      renderChart(transformedData);
-    } catch (error) {
-      console.error('Chyba při načítání dat:', error);
-    }
-  });
-  
-  function renderChart(data: ChartData[]) {
-    const chartData = {
-      labels: data.map((item) => item.Hodina),
-      datasets: [
-        {
-          label: 'Ceny za 24 hodin',
-          data: data.map((item) => {
-            const priceEuro = item['Eur/MWH'];
-            const exchangeRate = 25; // Předpokládaný kurz EUR/CZK
-            const priceCZK = priceEuro * exchangeRate;
-            return priceCZK;
-          }),
-          backgroundColor: data.map((item) => {
-            const price = item['Eur/MWH'];
-            if (price >= 0) {
-              return '#8DC63F'; // Zelená barva pro pozitivní ceny
-            } else {
-              return '#FF5C60'; // Červená barva pro negativní ceny
-            }
-          }),
-          borderColor: '#000000',
-          borderWidth: 1,
-          borderRadius: 8,
-        },
-      ],
-    };
-  
-    const chartOptions = {
-      responsive: true,
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
+});
+
+function renderChart() {
+  const chartData = {
+    labels: data.map((item) => item.Hodina),
+    datasets: [
+      {
+        label: 'Ceny za 24 hodin',
+        data: data.map((item) => {
+          const priceEuro = parseFloat(item['Eur/MWH']);
+          const exchangeRate = 25; // Předpokládaný kurz EUR/CZK
+          const priceCZK = priceEuro * exchangeRate;
+          return priceCZK;
+        }),
+        backgroundColor: data.map((item) => {
+          const price = parseFloat(item['Eur/MWH']);
+          if (price >= 0) {
+            return '#8DC63F'; // Zelená barva pro pozitivní ceny
+          } else {
+            return '#FF5C60'; // Červená barva pro negativní ceny
+          }
+        }),
+        borderColor: '#000000',
+        borderWidth: 1,
+        borderRadius: 8,
       },
-      plugins: {
-        tooltip: {
-          enabled: true, // Vypnutí tooltipu
-        },
-        legend: {
-          display: true, // Skrytí legendy
-        },
-        annotation: {
-          annotations: [
-            {
-              type: 'line',
-              mode: 'horizontal',
-              scaleID: 'y',
-              value: parseFloat(dailyMin),
-              borderColor: '#baf400',
-              borderWidth: 1,
-              label: {
-                enabled: true,
-                content: dailyMin,
-                backgroundColor: 'transparent',
-                font: {
-                  family: 'Euclid Circular A',
-                  size: 12,
-                  weight: 500,
-                  color: '#baf400',
-                },
-                yAdjust: -12, // Posunutí textu nahoru
-              },
-            },
-            {
-              type: 'line',
-              mode: 'horizontal',
-              scaleID: 'y',
-              value: parseFloat(currentPrice),
-              borderColor: '#000000',
-              borderWidth: 1,
-              label: {
-                enabled: true,
-                content: currentPrice,
-                backgroundColor: 'transparent',
-                font: {
-                  family: 'Euclid Circular A',
-                  size: 12,
-                  weight: 500,
-                  color: '#000000',
-                },
-                yAdjust: -12,
-              },
-            },
-            {
-              type: 'line',
-              mode: 'horizontal',
-              scaleID: 'y',
-              value: parseFloat(dailyMax),
-              borderColor: '#ff0000',
-              borderWidth: 1,
-              label: {
-                enabled: true,
-                content: dailyMax,
-                backgroundColor: 'transparent',
-                font: {
-                  family: 'Euclid Circular A',
-                  size: 12,
-                  weight: 500,
-                  color: '#ff0000',
-                },
-                yAdjust: -12,
-              },
-            },
-          ],
-        },
+    ],
+  };
+
+  const chartOptions = {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true,
       },
-    };
-  
-    const chartConfig = {
-      type: 'bar',
-      data: chartData,
-      options: chartOptions,
-    };
-  
-    const ctx = document.getElementById('chartCanvas');
-    if (ctx) {
-      new Chart(ctx, chartConfig);
-    }
+    },
+    plugins: {
+      tooltip: {
+        enabled: true, // Vypnutí tooltipu
+      },
+      legend: {
+        display: true, // Skrytí legendy
+      },
+      annotation: {
+        annotations: [
+          {
+            type: 'line',
+            mode: 'horizontal',
+            scaleID: 'y',
+            value: parseFloat(dailyMin),
+            borderColor: '#baf400',
+            borderWidth: 1,
+            label: {
+              enabled: true,
+              content: dailyMin,
+              backgroundColor: 'transparent',
+              font: {
+                family: 'Euclid Circular A',
+                size: 12,
+                weight: 500,
+                color: '#baf400',
+              },
+              yAdjust: -12, // Posunutí textu nahoru
+            },
+          },
+          {
+            type: 'line',
+            mode: 'horizontal',
+            scaleID: 'y',
+            value: parseFloat(currentPrice),
+            borderColor: '#000000',
+            borderWidth: 1,
+            label: {
+              enabled: true,
+              content: currentPrice,
+              backgroundColor: 'transparent',
+              font: {
+                family: 'Euclid Circular A',
+                size: 12,
+                weight: 500,
+                color: '#000000',
+              },
+              yAdjust: -12,
+            },
+          },
+          {
+            type: 'line',
+            mode: 'horizontal',
+            scaleID: 'y',
+            value: parseFloat(dailyMax),
+            borderColor: '#ff0000',
+            borderWidth: 1,
+            label: {
+              enabled: true,
+              content: dailyMax,
+              backgroundColor: 'transparent',
+              font: {
+                family: 'Euclid Circular A',
+                size: 12,
+                weight: 500,
+                color: '#ff0000',
+              },
+              yAdjust: -12,
+            },
+          },
+        ],
+      },
+    },
+  };
+
+  const chartConfig = {
+    type: 'bar',
+    data: chartData,
+    options: chartOptions,
+  };
+
+  const ctx = document.getElementById('chartCanvas');
+  if (ctx) {
+    new Chart(ctx, chartConfig);
   }
-  
-  function calculateDailyMin(data: ChartData[]) {
-    const prices = data.map((item) => item['Eur/MWH']);
+}
+
+  function calculateDailyMin(data: any[]) {
+    const prices = data.map((item) => parseFloat(item['Eur/MWH']));
     const minPrice = Math.min(...prices);
     return minPrice.toFixed(2);
   }
-  
-  function calculateCurrentPrice(data: ChartData[]) {
+
+  function calculateCurrentPrice(data: any[]) {
     const currentHour = new Date().getHours();
-    const currentPrice = data[currentHour - 1]['Eur/MWH'];
+    const currentPrice = parseFloat(data[currentHour - 1]['Eur/MWH']);
     return currentPrice.toFixed(2);
   }
-  
-  function calculateDailyMax(data: ChartData[]) {
-    const prices = data.map((item) => item['Eur/MWH']);
+
+  function calculateDailyMax(data: any[]) {
+    const prices = data.map((item) => parseFloat(item['Eur/MWH']));
     const maxPrice = Math.max(...prices);
     return maxPrice.toFixed(2);
   }
-  </script>
+</script>
 <div class="flex justify-center items-center h-screen">
   <div class="table bg-[#003941] h-[544px] w-[1020px] rounded-[56px] px-5 py-5">
     <!-- Horní část -->
